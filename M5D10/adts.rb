487e1f0aad0f54f3ac2bee72c86008d58f5f0233
rorro <ro.ladrondeguevara@gmail.com>
2022-05-10 09:41:31 -0300

    stack class added

--- M5D10/adts.rb
+++ M5D10/adts.rb
@@ -1,1 +1,19 @@
+class Stack
+    def initialize
+        @stack = []
+    end
+
+    def push(el)
+        @stack << el
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        return @stack[-1]
+    end
+
+end
 
