1f7fed551477f445b4bba0b5913e21cb8a2fd531
rorro <ro.ladrondeguevara@gmail.com>
2022-05-10 09:47:51 -0300

    queue class added

--- M5D10/adts.rb
+++ M5D10/adts.rb
@@ -17,3 +17,20 @@ class Stack
 
 end
 
+class Queue
+    def initialize
+        @queue = []
+    end
+
+    def enqueue(el)
+        @queue << el
+    end
+
+    def dequeue
+        @queue.shift
+    end
+
+    def peek
+        @queue[-1]
+    end
+end
