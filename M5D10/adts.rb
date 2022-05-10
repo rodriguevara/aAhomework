89f502e594a5885d20e3748313394c3a8e9a08de
rorro <ro.ladrondeguevara@gmail.com>
2022-05-10 10:15:28 -0300

    map class added

--- M5D10/adts.rb
+++ M5D10/adts.rb
@@ -34,3 +34,46 @@ class Queue
         @queue[-1]
     end
 end
+
+class Map
+    def initialize
+        @map = []
+    end
+
+    def set(key,value)
+        flag = false
+        @map.each do |sub|
+            if sub[0] == key
+                flag = true
+                sub[1] = value
+                return "key value pair modified"
+            end
+        end
+        @map << [key,value] if flag == false
+        flag = false
+        return "key value pair added"
+    end
+
+    def get(key)
+        @map.each do |sub|
+            if sub[0] == key
+                return sub
+            end
+        end
+        return "No key value pair was found with that key"
+    end
+
+    def delete(key)
+        @map.each.with_index do |sub,i|
+            if sub[0] == key
+                @map.delete_at(i)
+                return "Deleted #{sub} key value pair"
+            end
+        end
+        return "No key value pair was found with that key"
+    end
+
+    def show
+        return @map
+    end
+end
